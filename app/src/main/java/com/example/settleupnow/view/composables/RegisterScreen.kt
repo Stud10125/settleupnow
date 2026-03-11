9c16eeffd259ce7307eb58d6c8a859d1f6f078ca
10851074 <akshatakhadake2003@gmail.com>
2026-03-11 15:57:52 +0530

    structures

--- app/src/main/java/com/example/settleupnow/view/composables/RegisterScreen.kt
+++ app/src/main/java/com/example/settleupnow/view/composables/RegisterScreen.kt
@@ -1,4 +1,47 @@
-package com.example.settleupnow.view.composables;
+package com.example.settleupnow.view.composables
 
-public class RegisterScreen {
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.settleupnow.view.composables.ui.theme.SettleUpNowTheme
+
+class RegisterScreen : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SettleUpNowTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting2(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting2(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview2() {
+    SettleUpNowTheme {
+        Greeting2("Android")
+    }
 }
